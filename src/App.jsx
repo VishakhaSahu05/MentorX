a012d1df70731574552fda1179dffcfce6da1a69
VishakhaSahu05 <sahuvishakha756@gmail.com>
2025-12-14 20:10:20 +0530

    Setup Tailwind Css with Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 function App() {
   return (
-    <div>
-      <h1>MentorX Frontend Running 🚀</h1>
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-extrabold text-white mb-4">
+          MentorX 🚀
+        </h1>
+
+        <p className="text-gray-300 mb-6">
+          Tailwind CSS is successfully configured
+        </p>
+
+        <button className="px-6 py-2 rounded-lg bg-blue-600 text-white font-semibold hover:bg-blue-700 transition">
+          Get Started
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
